1fd0bc849c1c1eb2c3f49166448a24f2a1331997
Claire Nelson <claire_e_nelson@yahoo.co.uk>
2022-01-10 12:53:30 +0000

    updated fizzbuzz.rb and fizzbuzz_spec.rb

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,4 +1,13 @@
 require './lib/fizzbuzz'
+# return vaues dependent on whether input number is a 
+# multiple of 3 or 5 or 15. If none of these return input
 def fizzbuzz(number)
-    'fizz'
+    return "fizzbuzz" if (number % 5 == 0 && number % 3 == 0)
+    if number % 5 == 0
+      return "buzz"
+    elsif number % 3 == 0
+      return "fizz"
+    else
+      return number
+    end
 end
